3a5f6eb8ca2a3be63ecf7d6d0069f9b067c3f1b4
Tara Tandel <tara.tandel@icloud.com>
2018-07-10 00:33:14 +0430

    main page Add

---
+++ BazaarTestApp/BazaarTestApp/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BazaarTestApp
+//
+//  Created by Tara Tandel on 4/17/1397 AP.
+//  Copyright © 1397 Tara Tandel. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var movieName: UITextField!
+    @IBOutlet weak var search: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
