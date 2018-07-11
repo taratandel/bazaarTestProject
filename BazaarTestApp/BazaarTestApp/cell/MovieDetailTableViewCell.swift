4e56716720f38c4ab35a462755855d514af3123c
Tara Tandel <tara.tandel@icloud.com>
2018-07-11 11:32:11 +0430

    table View Cell Add

---
+++ BazaarTestApp/BazaarTestApp/cell/MovieDetailTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailTableViewCell.swift
+//  BazaarTestApp
+//
+//  Created by Tara Tandel on 4/20/1397 AP.
+//  Copyright © 1397 Tara Tandel. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleOfTheMovie: UILabel!
+    @IBOutlet weak var poster: UIImageView!
+    @IBOutlet weak var releaseDate: UILabel!
+    
+    @IBOutlet weak var movieOverViewDropDown: UIView!
+    @IBOutlet weak var movieOverView: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
